677bfd7c61ba94cab0c1e89a08aaf690ba119977
amol <ap195569@gmail.com>
2026-01-02 22:48:09 +0530

    amol branch code

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const home = () => {
   return (
     <div>
-                                       gi hub learing pupose applyed all coomds to master gihud
-   </div>
-  )
-}
+      {/* User Details Mobile */}
+      <div className="border-t pt-4">
+        <p className="font-bold text-lg">Amol Rajput</p>
+        <p className="text-sm text-gray-500">amol@example.com</p>
+        <button className="mt-3 w-full bg-linear-to-r from-purple-500 to-pink-500 text-white py-1.5 rounded-lg hover:scale-105 transition-all">
+          logiin and some emo{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default home
+export default home;
